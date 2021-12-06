ec09787b86617b7875daef9eaad09b12c6b18d8b
Rouven Malecki <rouven@robotriotgames.com>
2021-12-06 20:37:28 +0100

    Simplify

--- app/contexts/uncap/repayments/balance_repayments.rb
+++ app/contexts/uncap/repayments/balance_repayments.rb
@@ -12,18 +12,9 @@ module Uncap
         private
 
         def balance_repayments!(repayments, date_received, amount_received_from_payment)
-          overpaid_amount = get_overpaid_amount!(repayments, amount_received_from_payment)
-          reallocate_amount!(repayments, overpaid_amount, date_received)
-
-          repayments
-        end
-
-        def get_overpaid_amount!(repayments, amount_received_from_payment)
-          # YOUR CODE HERE  
-        end
-
-        def reallocate_amount!(repayments, amount, date_received)
           # YOUR CODE HERE
+          
+          repayments
         end
 
       end
