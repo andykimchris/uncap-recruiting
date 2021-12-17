acd08c55630d73d01b9624573830a51e8fa24c3b
andykimchris <andrew.muchiri97@gmail.com>
2021-12-17 13:57:54 +0300

    logic to balance excess repayments

--- app/contexts/uncap/repayments/balance_repayments.rb
+++ app/contexts/uncap/repayments/balance_repayments.rb
@@ -12,8 +12,30 @@ module Uncap
         private
 
         def balance_repayments!(repayments, date_received, amount_received_from_payment)
-          # YOUR CODE HERE
-          
+
+          excess_amounts = repayments.select { |repayment| repayment.amount_received > repayment.amount }
+           
+          if excess_amounts.any?
+            total_excess_payments = amount_received_from_payment + excess_amounts.pluck(:amount_received).sum
+            
+            repayments.each do |repayment|
+              if repayment.amount > repayment.amount_received
+                balancing_amount = repayment.amount - repayment.amount_received
+
+                unless total_excess_payments > balancing_amount
+                  break
+                end
+                
+                total_excess_payments -= balancing_amount
+                repayment.update(amount_received: repayment.amount, date_received: date_received)
+              end
+            end
+            repayments.last.update(amount_received: total_excess_payments) if total_excess_payments > 0
+          else
+            # NOTE: Assumption is to update last repayment or create new repayment
+            repayments.last.update(amount_received: amount_received_from_payment)
+          end
+        
           repayments
         end
 
